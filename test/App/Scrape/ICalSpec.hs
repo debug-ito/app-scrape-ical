5a8795ffc21aee37ddc685f81a8b919d1a4834f6
Toshio Ito <debug.ito@gmail.com>
2017-03-26 11:46:27 +0900

    add test case "sakura_matsuri.html". failing.

--- test/App/Scrape/ICalSpec.hs
+++ test/App/Scrape/ICalSpec.hs
@@ -1,5 +1,6 @@
 module App.Scrape.ICalSpec (main, spec) where
 
+import Data.Text (Text)
 import qualified Data.Text.IO as TIO
 import Data.Time (Day, fromGregorian)
 import App.Scrape.ICal
@@ -15,14 +16,25 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "scrapeEventChecker" $ do
+    let loadAndParse' = loadAndParse scrapeEventChecker "test/data/event-checker"
     it "should scrape date range" $ do
-      let file = "test/data/event-checker/american_fes_yokohama.html"
-      ret <- fmap (scrapeEventChecker file) $ TIO.readFile file
+      ret <- loadAndParse' "american_fes_yokohama.html"
       ret `shouldBe` ParseSuccess Event { eventName = "第10回アフリカンフェスティバルよこはま2017",
                                           eventWhen = (day 2017 3 24, day 2017 3 26),
                                           eventWhere = Just "横浜赤レンガ倉庫１号館",
                                           eventURI = Just "http://africanfestyokohama.com/"
                                         }
+    it "should scrape single date" $ do
+      ret <- loadAndParse' "sakura_matsuri.html"
+      ret `shouldBe` ParseSuccess Event { eventName = "新宿花園ゴールデン街 桜まつり2017",
+                                          eventWhen = (day 2017 4 16, day 2017 4 16),
+                                          eventWhere = Just "新宿ゴールデン街",
+                                          eventURI = Just "http://golden-gai.tokyo/sakura2017/"
+                                        }
 
 day :: Integer -> Int -> Int -> Day
 day = fromGregorian
+
+loadAndParse :: (String -> Text -> ParseResult a) -> String -> String -> IO (ParseResult a)
+loadAndParse parser dir filename = let file = dir ++ "/" ++ filename
+                                   in fmap (parser file) $ TIO.readFile file
