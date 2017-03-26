d155b4a7fe2fbc15238c7a2aa6f3563b56bb3856
Toshio Ito <debug.ito@gmail.com>
2017-03-26 11:34:15 +0900

    properly implement the test.

--- test/App/Scrape/ICalSpec.hs
+++ test/App/Scrape/ICalSpec.hs
@@ -1,7 +1,11 @@
 module App.Scrape.ICalSpec (main, spec) where
 
 import qualified Data.Text.IO as TIO
-import App.Scrape.ICal (scrapeEventChecker)
+import Data.Time (Day, fromGregorian)
+import App.Scrape.ICal
+  ( scrapeEventChecker,
+    Event(..), ParseResult(..)
+  )
 
 import Test.Hspec
 
@@ -14,5 +18,11 @@ spec = do
     it "should scrape date range" $ do
       let file = "test/data/event-checker/american_fes_yokohama.html"
       ret <- fmap (scrapeEventChecker file) $ TIO.readFile file
-      print ret
-      True `shouldBe` False
+      ret `shouldBe` ParseSuccess Event { eventName = "第10回アフリカンフェスティバルよこはま2017",
+                                          eventWhen = (day 2017 3 24, day 2017 3 26),
+                                          eventWhere = Just "横浜赤レンガ倉庫１号館",
+                                          eventURI = Just "http://africanfestyokohama.com/"
+                                        }
+
+day :: Integer -> Int -> Int -> Day
+day = fromGregorian
