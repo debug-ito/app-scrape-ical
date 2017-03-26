60bb1ea6faf4e467f7c18662dfe1d3b9d944dfcd
Toshio Ito <debug.ito@gmail.com>
2017-03-26 11:16:27 +0900

    add test. failing.

--- test/App/Scrape/ICalSpec.hs
+++ test/App/Scrape/ICalSpec.hs
@@ -1,5 +1,8 @@
 module App.Scrape.ICalSpec (main, spec) where
 
+import qualified Data.Text.IO as TIO
+import App.Scrape.ICal (scrapeEventChecker)
+
 import Test.Hspec
 
 main :: IO ()
@@ -7,6 +10,9 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "someFunction" $ do
-    it "should work fine" $ do
+  describe "scrapeEventChecker" $ do
+    it "should scrape date range" $ do
+      let file = "test/data/event-checker/american_fes_yokohama.html"
+      ret <- fmap (scrapeEventChecker file) $ TIO.readFile file
+      print ret
       True `shouldBe` False
